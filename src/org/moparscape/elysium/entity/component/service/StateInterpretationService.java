022e98c326963a63ecd026881ec2286357228f74
Joe0 <joe.pritzel0@gmail.com>
2012-02-08 15:53:28 -0600

    Added some documentation to StateInterpretationService

--- src/org/moparscape/elysium/entity/component/service/StateInterpretationService.java
+++ src/org/moparscape/elysium/entity/component/service/StateInterpretationService.java
@@ -2,10 +2,23 @@ package org.moparscape.elysium.entity.component.service;
 
 import org.moparscape.elysium.entity.component.State;
 
+/**
+ * Used to interpret the state of entities.
+ * 
+ * @author Joe Pritzel
+ * 
+ */
 public class StateInterpretationService {
 
-	public static boolean canLogout(State component) {
-		switch (component.getState()) {
+	/**
+	 * Determines if an entity is allowed to log out based on its state.
+	 * 
+	 * @param s
+	 *            - The state to check.
+	 * @return Returns true if the entity can log out.
+	 */
+	public static boolean canLogout(State s) {
+		switch (s.getState()) {
 		case BUSY:
 		case IN_COMBAT:
 			return false;
